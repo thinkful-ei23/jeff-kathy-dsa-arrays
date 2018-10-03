065699f7b4a41824df354714fca041ce2ee3676e
Jeff Maxwell <maxjeffwell@gmail.com>
2018-10-03 12:39:40 -0400

    added memory.js and array.js

---
+++ array.js
@@ -0,0 +1,90 @@
+const memoryClass = require('./memory');
+const memory = new memoryClass();
+
+
+class Array {
+    constructor() {
+        this.length = 0;
+        this.ptr = memory.allocate(this.length);
+        this._capacity = 0;
+    }
+
+    push(value) {
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
+        memory.set(this.ptr + this.length, value); // changes array item value at ptr
+        this.length++; //increases length of array
+    }
+
+    _resize(size) {
+        const oldPtr = this.ptr;
+        this.ptr = memory.allocate(size);
+        if (this.ptr === null) {
+            throw new Error('Out of memory');
+        }
+        memory.copy(this.ptr, oldPtr, this.length);
+        memory.free(oldPtr); // free unused memory
+        this._capacity = size;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        console.log(this.ptr + index);
+        return memory.get(this.ptr + index);
+    }
+
+    pop() {
+        if (this.length == 0) {
+            throw new Error('Index error');
+        }
+        const value = memory.get(this.ptr + this.length - 1);
+        this.length--;
+        return value;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
+
+        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+        memory.set(this.ptr + index, value);
+        this.length++;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+        this.length--;
+    }
+
+}
+
+function main(){
+
+    Array.SIZE_RATIO = 3;
+
+    //create an instance of the array class
+    let arr = new Array();
+
+    //add an item to the array
+    arr.push(3);
+
+    console.log(arr);
+    arr.get(0);
+}
+
+main();
+
+module.exports = Array;
+
+
